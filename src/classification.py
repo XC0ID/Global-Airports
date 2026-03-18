17b3fa67082121ae1ac2de5fc4c7baf5a12e9f18
Cold <maulik10gajera@gmail.com>
2026-03-18 17:05:32 +0530

    classification

---
+++ src/classification.py
@@ -0,0 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    return acc
